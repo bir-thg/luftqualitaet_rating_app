fadf7586429d4dcecf112c6201fbbc32f398cd2c
Bernd Birkicht <62995369+bir-thg@users.noreply.github.com>
2024-05-26 18:13:23 +0200

    changed port (later also url) and deletd one dart-file

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _rating = 1;
   bool _isSending = false;
-  String serverUrl = 'http://192.168.178.58:3000/rating';
+  String serverUrl = 'http://192.168.178.58:40081/rating';
 
   void _setRating(int rating) {
     setState(() {
