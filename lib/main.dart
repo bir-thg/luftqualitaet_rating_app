9bb433500acd261323e64f7d2fe9a7d710a9c9d9
Bernd Birkicht <62995369+bir-thg@users.noreply.github.com>
2024-05-26 14:58:52 +0200

    New version with asynchronous handling of geolocation, which is better than synchronous handling. With geocode-Plugin now also retrieving ZIP, Countrycode, Street, ... Therefor additionally more data, which is stored in the database (serverside). Also different ways of hnadling Linux and Android/iOS. Now Correct handling of http for Android (Android Manifest).

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:flutter/material.dart' show AppBar, BuildContext, Center, CircularProgressIndicator, Colors, Column, ElevatedButton, Icon, IconButton, Icons, MainAxisAlignment, MaterialApp, Row, Scaffold, ScaffoldMessenger, SizedBox, SnackBar, State, StatefulWidget, StatelessWidget, Text, ThemeData, Widget, runApp;
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,21 +18,22 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-// ignore: use_key_in_widget_constructors
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _rating = 1;
   bool _isSending = false;
+  String serverUrl = 'http://192.168.178.58:3000/rating';
 
   void _setRating(int rating) {
     setState(() {
@@ -42,27 +46,60 @@ class _MyHomePageState extends State<MyHomePage> {
       _isSending = true;
     });
 
+    try {
+      var url = Uri.parse(serverUrl);
+      http.Response response;
 
+      if (Platform.isLinux) {
+        // Auf Linux nur die Bewertung senden
+        response = await http.post(url, body: {
+          'rating': _rating.toString(),
+        });
+      } else {
+        // Auf anderen Plattformen Geokoordinaten ermitteln
+        bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+        if (!serviceEnabled) {
+          throw 'Location services are disabled.';
+        }
 
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        LocationPermission permission = await Geolocator.checkPermission();
+        if (permission == LocationPermission.denied) {
+          permission = await Geolocator.requestPermission();
+          if (permission == LocationPermission.denied) {
+            throw 'Location permissions are denied';
+          }
+        }
 
-    var url = Uri.parse('http://192.168.178.58:3000/rating');
-    var response = await http.post(url, body: {
-      'rating': _rating.toString(),
-      'latitude': position.latitude.toString(),
-      'longitude': position.longitude.toString(),
-    });
+        if (permission == LocationPermission.deniedForever) {
+          throw 'Location permissions are permanently denied, we cannot request permissions.';
+        }
 
-    setState(() {
-      _isSending = false;
-    });
+        Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
+        Placemark placemark = placemarks[0];
 
-    if (response.statusCode == 200) {
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Daten erfolgreich gesendet!')));
-    } else {
-      // ignore: use_build_context_synchronously
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Fehler beim Senden der Daten')));
+        response = await http.post(url, body: {
+          'rating': _rating.toString(),
+          'latitude': position.latitude.toString(),
+          'longitude': position.longitude.toString(),
+          'country': placemark.country ?? '',
+          'locality': placemark.locality ?? '',
+          'street': placemark.street ?? '',
+          'postalCode': placemark.postalCode ?? '',
+        });
+      }
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Daten erfolgreich gesendet!')));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Fehler beim Senden der Daten')));
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Fehler: $e')));
+    } finally {
+      setState(() {
+        _isSending = false;
+      });
     }
   }
 
